930cac1eeda1094d98999e0a01041761c125d59d
Byungwoo Choi <cbw4186@naver.com>
2024-06-24 16:50:41 +0900

    #1 React_Init
    리액트 초기 설정 및 vite설치

---
+++ front/NicknameProject/src/pages/InfoInput.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Background = styled.div`
+  background-color: #1554d1;
+`;
+const InfoInput = () => {
+  return (
+    <Background>
+        <h1>Info Input</h1>
+    </Background>
+  );
+}
+
+export default InfoInput;
